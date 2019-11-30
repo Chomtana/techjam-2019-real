f48632fda06e8c273794de2f6e5c733944c37b8f
Chomtana <Chomtana001@gmail.com>
2019-11-30 17:21:17 +0700

    add test

--- tests/robot.test.js
+++ tests/robot.test.js
@@ -18,9 +18,13 @@ test("dist", async () => {
 
 test("alien1", async () => {
     setRobotPos(1, {x:0, y:0});
+    expect(getRobotPos(1).position).toEqual({x:0, y:0});
     newAlien("abc", 1, 3.162);
     setRobotPos(1, {x:5, y:-2});
+    expect(getRobotPos(1).position).toEqual({x:5, y:-2});
     newAlien("abc", 1, 2.236);
     setRobotPos(2, {x:3, y:-3});
+    expect(getRobotPos(1).position).toEqual({x:5, y:-2});
+    expect(getRobotPos(2).position).toEqual({x:3, y:-3});
     newAlien("abc", 2, 2.000);
 })
