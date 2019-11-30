4f9257dae7e47c6c560e23cf359cfc52c5dd1fa1
Chomtana <Chomtana001@gmail.com>
2019-11-30 16:04:20 +0700

    add robot

--- src/api/robot.js
+++ src/api/robot.js
@@ -1,4 +1,13 @@
+let robots = {};
+
 function dist(first, second) {
+  if (typeof first === "string") {
+    first = parseInt(getRobotPos(/^robot#([1-9][0-9]*)$/.exec(first)[1]))
+  }
+  if (typeof second === "string") {
+    second = parseInt(getRobotPos(/^robot#([1-9][0-9]*)$/.exec(second)[1]))
+  }
+
   if (isNaN(parseFloat(first.x)) || isNaN(parseFloat(first.y)) || isNaN(parseFloat(second.x)) || isNaN(parseFloat(second.y))) throw "NaN";
   
   first.x = parseFloat(first.x);
@@ -10,6 +19,10 @@ function dist(first, second) {
   return {distance: parseFloat(Math.sqrt((second.x-first.x)*(second.x-first.x) + (second.y-first.y)*(second.y-first.y)).toFixed(4))};
 }
 
+function setRobotPos(id, pos) {
+  robots[id] = pos;
+}
+
 function api_robot(app) {
   app.post("/distance", async (req, res) => {
     try {
@@ -19,6 +32,16 @@ function api_robot(app) {
       res.status(400).send();
     }
   });
+
+  app.put("/robot/:id/position", async (req, res) => {
+    try {
+      setRobotPos(req.params.id, req.body.position)
+      res.sendStatus(204);
+    } catch (err) {
+      console.error(err);
+      res.status(400).send();
+    }
+  });
 }
 
-module.exports = {api_robot, dist}
+module.exports = {api_robot, dist, setRobotPos}
